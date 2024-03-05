f1ffa056f5dcae918744263183c6e08cb4927de5
JumpersLee <151594786+thisgames@users.noreply.github.com>
2024-03-05 22:02:26 +0100

    php html notes alpha

---
+++ Assets/css/Сценарий Windows.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Creating files in dp0 directory...
+echo. > "%~dp0\base.css"
+echo. > "%~dp0\layout.css"
+echo. > "%~dp0\form.css"
+echo. > "%~dp0\notes.css"
+echo. > "%~dp0\modal.css"
+echo Files created successfully.
